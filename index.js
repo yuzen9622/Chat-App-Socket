1ba3821e5e23bb446d0e3a764b6cf17583a2e798
yuzen622 <125558430+yuzen622@users.noreply.github.com>
2024-04-20 09:35:12 +0800

    Initial commit

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const { Server } = require("socket.io");
 const io = new Server({ cors: "https://yuzen9622.github.io" });
 let onlineUsers = []
 io.on("connection", (socket) => {
-
+   
+   
     socket.on("addNewUser", (userId) => {
+       
         if (userId === null) return
         !onlineUsers.some((user) => user.userId === userId) &&
             onlineUsers.push({
@@ -14,13 +16,9 @@ io.on("connection", (socket) => {
             })
 
         io.emit("getonlineUsers", onlineUsers)
+        console.log(onlineUsers)
     })
-    socket.on("InchatUser", (userChat) => {
-        const user = onlineUsers.find((user1) => user1.userId === userChat.recipientId);
-        if (user) {
-            io.to(user.socketId).emit("getInChatUser", userChat)
-        }
-    })
+
     socket.on("sendMessage", (message) => {
         console.log(onlineUsers.find((user1) => user1.userId == message.recipientId))
         const user = onlineUsers.find((user1) => user1.userId === message.recipientId);
@@ -36,6 +34,45 @@ io.on("connection", (socket) => {
         }
 
     })
+
+socket.on("typing",(req)=>{
+    
+    const user = onlineUsers.find((user1) => user1.userId == req.recipientId);
+    console.log(user)
+    if(user){
+        io.to(user.socketId).emit("userTyping",{id:req.id,istype:req?.status})
+    }
+    
+
+})
+
+
+socket.on("callUser", (data) => {
+    const calluser = onlineUsers?.find((users) => users.userId === data.id);
+ if(!calluser)return io.to(data.from).emit("callAccepted",false)
+    if (calluser)
+    console.log("send call")
+      io.to(calluser.socketId).emit("getCall", {
+   
+        signal: data.signalData,
+        from: data.from,
+        name: data.name,
+        iscamera:data.iscamera
+      });
+  });
+
+  socket.on("answerCall", (data) => {
+    const calluser = onlineUsers?.find((users) => users.userId === data.to);
+    if(calluser)
+    io.to(calluser.socketId).emit("callAccepted", data.signal);
+  });
+
+
+socket.on("callend",(data)=>{
+    const calluser = onlineUsers?.find((users) => users.userId === data);
+    if(calluser) 
+    io.to(calluser.socketId).emit("callEnded");
+})
 
     socket.on("disconnect", () => {
         onlineUsers = onlineUsers.filter(user => user.socketId !== socket.id);
